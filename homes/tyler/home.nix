8ca8b4391449f721db6d73979d0a2c3fd83a0195
Tyler <tylerfaulhaber@gmail.com>
2025-11-16 22:05:46 -0500

    sdfsdfsdf

--- homes/tyler/home.nix
+++ homes/tyler/home.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }: { 
-
+ 
+  let
+    nvimConfig = builtins.path { path = ./nvim-config; };
+  in  {
     home.username = "tyler";
     home.homeDirectory = "/home/tyler";
     home.packages = with pkgs; [
@@ -8,7 +11,6 @@
 
     # Nvim configuration
     programs.neovim.enable = true;
-    nvimConfig = builtins.path { path = ./nvim-config; };
     home.file.".config/nvim".source = nvimConfig;
 
     programs.home-manager.enable = true;
