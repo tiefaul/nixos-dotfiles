7084f1ab99547e42de7305c53e02499b13936aa2
Tyler <tylerfaulhaber@gmail.com>
2025-11-16 22:34:54 -0500

    sdfsdf

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.tyler = ./homes/tyler/home.nix 
+            home-manager.users.tyler = ./homes/tyler/home.nix; 
           }
         ];
       };
