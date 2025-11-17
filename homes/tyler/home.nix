759799d59d58a5f15a6ad5636235af0b9addfd8f
Tyler <tylerfaulhaber@gmail.com>
2025-11-16 19:31:06 -0500

    dsfsdfsdf

--- homes/tyler/home.nix
+++ homes/tyler/home.nix
@@ -2,14 +2,13 @@
 
   home.username = "tyler";
   home.homeDirectory = "/home/tyler";
-
   home.packages = with pkgs [
     python314
   ];
 
   # Nvim configuration
   programs.neovim.enable = true;
-  home.file.".config/nvim".source = "./nvim-config/.";
+#  home.file.".config/nvim".source = "./nvim-config/.";
 
   programs.home-manager.enable = true;
   
