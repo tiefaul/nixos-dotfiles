7ef6f8ac84c150f06c6d14f105f2ff99192d4fa4
Tyler <tylerfaulhaber@gmail.com>
2025-11-16 22:24:09 -0500

    sdfsdfdsf

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.tyler = ./homes/tyler/home.nix;
-	    nvimConfigPath = nvimConfig
+	    nvimConfigPath = nvimConfig;
           }
         ];
       };
