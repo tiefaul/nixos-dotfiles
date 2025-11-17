c0f1b01e7b4b31c170e05319c880a4b5440fcfc2
Tyler <tylerfaulhaber@gmail.com>
2025-11-16 21:45:56 -0500

    sdfsdfd

--- homes/tyler/home.nix
+++ homes/tyler/home.nix
@@ -1,17 +1,23 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: 
 
-  home.username = "tyler";
-  home.homeDirectory = "/home/tyler";
-  home.packages = with pkgs; [
-    python314
-  ];
+  let
+    nvim-config = builtins.path {
+      path = ./nvim-config/.;
+      name = "nvim-config";
+    };
+  in {
+    home.username = "tyler";
+    home.homeDirectory = "/home/tyler";
+    home.packages = with pkgs; [
+      python314
+    ];
 
-  # Nvim configuration
-  programs.neovim.enable = true;
-  home.file.".config/nvim".source = ./nvim-config/.;
+    # Nvim configuration
+    programs.neovim.enable = true;
+    home.file.".config/nvim".source = nvim-config;
 
-  programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
   
-  home.stateVersion = "25.05";
+    home.stateVersion = "25.05";
 }
 
