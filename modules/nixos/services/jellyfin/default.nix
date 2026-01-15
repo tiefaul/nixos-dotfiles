5a8dc74c266004951290109a4d38aa7068878adf
Tyler <tylerfaulhaber@gmail.com>
2026-01-15 14:37:46 -0500

    added jellyfin module

---
+++ modules/nixos/services/jellyfin/default.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... }:
+let
+  cfg = config.tyler-space.services.jellyfin;
+in
+  {
+    options.tyler-space.services.jellyfin = {
+      enable = lib.mkEnableOption "Enable jellyfin";
+    };
+
+    config = lib.mkIf cfg.enable {
+      services.jellyfin.enable = true;
+    };
+}
