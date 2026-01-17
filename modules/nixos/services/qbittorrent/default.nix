778b5460b17306aa80d88f3a16d724abe98a115c
Tyler <tylerfaulhaber@gmail.com>
2026-01-17 00:46:01 -0500

    added something

---
+++ modules/nixos/services/qbittorrent/default.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.tyler-space.services.qbittorrent;
+in 
+  {
+    options.tyler-space.services.qbittorrent = {
+      enable = lib.mkEnableOption "Enable qbittorrent";
+    };
+
+    config = lib.mkIf cfg.enable {
+      services.qbittorrent = {
+        enable = true;
+        openFirewall = true;
+      };
+    };
+  }
