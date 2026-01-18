a853ddc114687744567f573f50cc983b524201b9
Tyler <tylerfaulhaber@gmail.com>
2026-01-17 19:21:30 -0500

    updated nvim config

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -2,12 +2,6 @@
 
 { pkgs, config, lib, ... }:
 let
-  nvim_config = pkgs.fetchFromGitHub {
-    owner = "tiefaul";
-    repo  = "nvim";
-    rev   = "main";          # tag, branch, or commit SHA
-    hash  = "sha256-xyCZcFYdTLbqpiomirATMsksygwNOgIZs07Y3qtAmWQ=";
-  };
   cfg = config.tyler-space.nvim;
 in {
   options.tyler-space.nvim = {
@@ -16,7 +10,7 @@ in {
 
   config = lib.mkIf cfg.enable {
      programs.neovim.enable = true;
-     home.file.".config/nvim".source = nvim_config;
+     home.file.".config/nvim".source = ./nvim/.;
   };
 }
 
